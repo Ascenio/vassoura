f4f8229586880043ee4ae04d991f68d557710bba
Ascênio Sanderson <ascenio@hotmail.com>
2021-04-30 10:14:27 -0300

    feat: initial commit

---
+++ bin/vassoura.dart
@@ -0,0 +1,5 @@
+import 'package:vassoura/vassoura.dart' as vassoura;
+
+void main(List<String> arguments) {
+  print('Hello world: ${vassoura.calculate()}!');
+}
---
+++ test/vassoura_test.dart
@@ -0,0 +1,8 @@
+import 'package:vassoura/vassoura.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
