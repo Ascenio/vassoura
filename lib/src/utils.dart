fe1d7b16635e69680a1a1e92dd67652cedc3f4c3
Ascênio Sanderson <ascenio@hotmail.com>
2021-05-04 21:45:42 -0300

    refactor: removes unused function

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 bool programHasMain(String program) {
   final regex = RegExp(r'(void)? main\s*\(');
   return regex.hasMatch(program);
@@ -7,8 +5,4 @@ bool programHasMain(String program) {
 
 bool isAnAbsoluteImport(String importPath) {
   return importPath.startsWith('package:');
-}
-
-Future<bool> filesAreEqual(File first, File second) {
-  return FileSystemEntity.identical(first.path, second.path);
 }
